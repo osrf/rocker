c4daf13b56f1fa488d52efc76a210031b79700f6
Tully Foote <tfoote@osrfoundation.org>
2019-01-09 17:59:17 -0800

    0.1.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 kwargs = {
     'name': 'rocker',
-    'version': '0.1.2',
+    'version': '0.1.3',
     'packages': ['rocker'],
     'package_dir': {'': 'src'},
     'package_data': {'rocker': ['templates/*.em']},
