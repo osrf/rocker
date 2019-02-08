fd3215aacf219b6c53030fcefc4667a610bd731e
Tully Foote <tfoote@osrfoundation.org>
2019-02-07 16:14:21 -0800

    add coverage for extensions

--- test/test_extension.py
+++ test/test_extension.py
@@ -23,7 +23,13 @@ from pathlib import Path
 
 
 from rocker.cli import list_plugins
+from rocker.extensions import name_to_argument
 
+class ExtensionsTest(unittest.TestCase):
+    def test_name_to_argument(self):
+        self.assertEqual(name_to_argument('asdf'), '--asdf')
+        self.assertEqual(name_to_argument('as_df'), '--as-df')
+        self.assertEqual(name_to_argument('as-df'), '--as-df')
 
 class HomeExtensionTest(unittest.TestCase):
 
@@ -38,7 +44,7 @@ class HomeExtensionTest(unittest.TestCase):
         # "em.Error: interpreter stdout proxy lost"
         em.Interpreter._wasProxyInstalled = False
 
-    def test_user_extension(self):
+    def test_home_extension(self):
         plugins = list_plugins()
         home_plugin = plugins['home']
         self.assertEqual(home_plugin.get_name(), 'home')
@@ -87,3 +93,77 @@ class UserExtensionTest(unittest.TestCase):
 
         self.assertEqual(p.get_preamble(mock_cliargs), '')
         self.assertEqual(p.get_docker_args(mock_cliargs), '')
+
+
+EXPECTED_PULSE_SNIPPET = """RUN mkdir -p /etc/pulse
+RUN echo '\\n\\
+# Connect to the hosts server using the mounted UNIX socket\\n\\
+default-server = unix:/run/user/1000/pulse/native\\n\\
+\\n\\
+# Prevent a server running in the container\\n\\
+autospawn = no\\n\\
+daemon-binary = /bin/true\\n\\
+\\n\\
+# Prevent the use of shared memory\\n\\
+enable-shm = false\\n\\
+\\n'\\
+> /etc/pulse/client.conf
+"""
+
+class PulseExtensionTest(unittest.TestCase):
+
+    def setUp(self):
+        # Work around interference between empy Interpreter
+        # stdout proxy and test runner. empy installs a proxy on stdout
+        # to be able to capture the information.
+        # And the test runner creates a new stdout object for each test.
+        # This breaks empy as it assumes that the proxy has persistent
+        # between instances of the Interpreter class
+        # empy will error with the exception
+        # "em.Error: interpreter stdout proxy lost"
+        em.Interpreter._wasProxyInstalled = False
+
+    def test_pulse_extension(self):
+        plugins = list_plugins()
+        pulse_plugin = plugins['pulse']
+        self.assertEqual(pulse_plugin.get_name(), 'pulse')
+
+        p = pulse_plugin()
+        
+        mock_cliargs = {}
+
+        self.assertEqual(p.get_snippet(mock_cliargs), EXPECTED_PULSE_SNIPPET)
+        self.assertEqual(p.get_preamble(mock_cliargs), '')
+
+EXPECTED_DEV_HELPERS_SNIPPET = """# workspace development helpers
+RUN apt-get update \\
+ && apt-get install -y \\
+    byobu \\
+    emacs \\
+ && apt-get clean
+"""
+
+class DevHelpersExtensionTest(unittest.TestCase):
+
+    def setUp(self):
+        # Work around interference between empy Interpreter
+        # stdout proxy and test runner. empy installs a proxy on stdout
+        # to be able to capture the information.
+        # And the test runner creates a new stdout object for each test.
+        # This breaks empy as it assumes that the proxy has persistent
+        # between instances of the Interpreter class
+        # empy will error with the exception
+        # "em.Error: interpreter stdout proxy lost"
+        em.Interpreter._wasProxyInstalled = False
+
+    def test_pulse_extension(self):
+        plugins = list_plugins()
+        pulse_plugin = plugins['dev_helpers']
+        self.assertEqual(pulse_plugin.get_name(), 'dev_helpers')
+
+        p = pulse_plugin()
+        
+        mock_cliargs = {}
+
+        self.assertEqual(p.get_snippet(mock_cliargs), EXPECTED_DEV_HELPERS_SNIPPET)
+        self.assertEqual(p.get_preamble(mock_cliargs), '')
