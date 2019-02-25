6d69d0ea671fe54e6901cb4b66bb77a5b3406233
Tully Foote <tfoote@osrfoundation.org>
2019-02-25 15:02:12 -0800

    Separate x11 from nvidia extension for use on non-nvidia platforms

--- test/test_nvidia.py
+++ test/test_nvidia.py
@@ -28,6 +28,72 @@ from rocker.cli import list_plugins
 from rocker.core import get_docker_client
 from test_extension import plugin_load_parser_correctly
 
+
+class X11Test(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        client = get_docker_client()
+        self.dockerfile_tags = []
+        for distro_version in ['xenial', 'bionic']:
+            dockerfile = """
+FROM ubuntu:%(distro_version)s
+
+RUN apt-get update && apt-get install x11-utils -y && apt-get clean
+
+CMD xdpyinfo
+"""
+            dockerfile_tag = 'testfixture_%s_x11_validate' % distro_version
+            iof = StringIO((dockerfile % locals()).encode())
+            im = client.build(fileobj = iof, tag=dockerfile_tag)
+            for e in im:
+                pass
+                #print(e)
+            self.dockerfile_tags.append(dockerfile_tag)
+
+    def setUp(self):
+        # Work around interference between empy Interpreter
+        # stdout proxy and test runner. empy installs a proxy on stdout
+        # to be able to capture the information.
+        # And the test runner creates a new stdout object for each test.
+        # This breaks empy as it assumes that the proxy has persistent
+        # between instances of the Interpreter class
+        # empy will error with the exception
+        # "em.Error: interpreter stdout proxy lost"
+        em.Interpreter._wasProxyInstalled = False
+
+    def test_x11_extension_basic(self):
+        plugins = list_plugins()
+        x11_plugin = plugins['x11']
+        self.assertEqual(x11_plugin.get_name(), 'x11')
+        self.assertTrue(plugin_load_parser_correctly(x11_plugin))
+        
+        p = x11_plugin()
+        mock_cliargs = {'base_image': 'ubuntu:xenial'}
+
+        docker_args = p.get_docker_args(mock_cliargs)
+        self.assertIn(' -e DISPLAY -e TERM', docker_args)
+        self.assertIn(' -e QT_X11_NO_MITSHM=1', docker_args)
+        self.assertIn(' -e XAUTHORITY=', docker_args)
+        self.assertIn(' -v /tmp/.X11-unix:/tmp/.X11-unix ', docker_args)
+        self.assertIn(' -v /etc/localtime:/etc/localtime:ro ', docker_args)
+
+
+    def test_no_x11_xpdyinfo(self):
+        for tag in self.dockerfile_tags:
+            dig = DockerImageGenerator([], {}, tag)
+            self.assertEqual(dig.build(), 0)
+            self.assertNotEqual(dig.run(), 0)
+
+    def test_x11_xpdyinfo(self):
+        plugins = list_plugins()
+        desired_plugins = ['x11']
+        active_extensions = [e() for e in plugins.values() if e.get_name() in desired_plugins]
+        for tag in self.dockerfile_tags:
+            dig = DockerImageGenerator(active_extensions, {}, tag)
+            self.assertEqual(dig.build(), 0)
+            self.assertEqual(dig.run(), 0)
+
+
 class NvidiaTest(unittest.TestCase):
     @classmethod
     def setUpClass(self):
@@ -63,7 +129,7 @@ CMD glmark2 --validate
     def test_nvidia_extension_basic(self):
         plugins = list_plugins()
         nvidia_plugin = plugins['nvidia']
-        self.assertEqual(nvidia_plugin.get_name(), 'nvidia')    
+        self.assertEqual(nvidia_plugin.get_name(), 'nvidia')
         self.assertTrue(plugin_load_parser_correctly(nvidia_plugin))
         
         p = nvidia_plugin()
@@ -91,15 +157,15 @@ CMD glmark2 --validate
         self.assertIn('FROM nvidia/opengl:1.0-glvnd-devel-', preamble)
 
         docker_args = p.get_docker_args(mock_cliargs)
-        self.assertIn(' -e DISPLAY -e TERM', docker_args)
-        self.assertIn(' -e QT_X11_NO_MITSHM=1', docker_args)
-        self.assertIn(' -e XAUTHORITY=', docker_args)
-        self.assertIn(' -v /tmp/.X11-unix:/tmp/.X11-unix ', docker_args)
-        self.assertIn(' -v /etc/localtime:/etc/localtime:ro ', docker_args)
+        #TODO(tfoote) restore with #37 self.assertIn(' -e DISPLAY -e TERM', docker_args)
+        #TODO(tfoote) restore with #37 self.assertIn(' -e QT_X11_NO_MITSHM=1', docker_args)
+        #TODO(tfoote) restore with #37 self.assertIn(' -e XAUTHORITY=', docker_args)
+        #TODO(tfoote) restore with #37 self.assertIn(' -v /tmp/.X11-unix:/tmp/.X11-unix ', docker_args)
+        #TODO(tfoote) restore with #37 self.assertIn(' -v /etc/localtime:/etc/localtime:ro ', docker_args)
         self.assertIn(' --runtime=nvidia ', docker_args)
         self.assertIn(' --security-opt seccomp=unconfined', docker_args)
 
-    
+
     def test_no_nvidia_glmark2(self):
         for tag in self.dockerfile_tags:
             dig = DockerImageGenerator([], {}, tag)
@@ -108,7 +174,7 @@ CMD glmark2 --validate
 
     def test_nvidia_glmark2(self):
         plugins = list_plugins()
-        desired_plugins = ['nvidia', 'user']
+        desired_plugins = ['x11', 'nvidia', 'user'] #TODO(Tfoote) encode the x11 dependency into the plugin and remove from test here
         active_extensions = [e() for e in plugins.values() if e.get_name() in desired_plugins]
         for tag in self.dockerfile_tags:
             dig = DockerImageGenerator(active_extensions, {}, tag)
