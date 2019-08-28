39118bb877553364f70e082be24aaf07559f7ef4
Tully Foote <tfoote@osrfoundation.org>
2019-08-27 17:13:33 -0700

    remove unused imports
    Signed-off-by: Tully Foote <tfoote@osrfoundation.org>

--- src/rocker/extensions.py
+++ src/rocker/extensions.py
@@ -22,8 +22,6 @@ from pathlib import Path
 import subprocess
 import sys
 
-from .os_detector import build_detector_image
-from .os_detector import detect_os
 
 def name_to_argument(name):
     return '--%s' % name.replace('_', '-')
