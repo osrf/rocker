07ce911c2f1b3892055c9c9854570fe48971d6e5
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2021-04-18 13:26:02 -0400

    [mount] Add unit test.
    [test][mount] Add a test case for multiple mount points.

---
+++ test/test_mount.py
@@ -0,0 +1,69 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+import unittest
+
+from rocker.core import list_plugins
+from rocker.mount_extension import Mount
+
+
+class MountTest(unittest.TestCase):
+    def setUp(self):
+        self._instance = Mount()
+        self._curr_path = os.path.abspath(os.path.curdir)
+        self._virtual_path = "/path/in/container"
+
+    def _test_equals_args(self, mock_cliargs, expected):
+        """
+        @type mock_cliargs: { str: [[str]] }
+        @type expected: [[str]]
+        """
+        print("DEBUG: 'mock_cliargs' {}\n\t'expected': {}".format(mock_cliargs, expected))
+        docker_args = self._instance.get_docker_args(mock_cliargs)
+        print("DEBUG: Resulted docker_args: {}".format(docker_args))
+        for arg_expected in expected:
+            # Whitespace at the beginning is needed.
+            complete_expected = " {} {}".format(Mount.ARG_DOCKER_VOLUME, arg_expected[0])
+            self.assertTrue(complete_expected in docker_args)
+
+    def test_args_single(self):
+        """Passing source path"""
+        arg = [[self._curr_path]]
+        expected = [['{}:{}'.format(self._curr_path, self._curr_path)]]
+        mock_cliargs = {Mount.name: arg}
+        self._test_equals_args(mock_cliargs, expected)
+
+    def test_args_twopaths(self):
+        """Passing source path, dest path"""
+        arg = ["{}:{}".format(self._curr_path, self._virtual_path)]
+        mock_cliargs = {Mount.name: [arg]}
+        self._test_equals_args(mock_cliargs, arg)
+
+    def test_args_twopaths_opt(self):
+        """Passing source path, dest path, and Docker's mount option"""
+        arg = ["{}:{}:ro".format(self._curr_path, self._virtual_path)]
+        mock_cliargs = {Mount.name: [arg]}
+        self._test_equals_args(mock_cliargs, arg)
+
+    def test_args_two_volumes(self):
+        """Multiple mount points"""
+        arg_first = ["{}:{}:ro".format(self._curr_path, self._virtual_path)]
+        arg_second = ["/tmp:{}".format(os.path.join(self._virtual_path, "tmp"))]
+        args = [arg_first, arg_second]
+        mock_cliargs = {Mount.name: args}
+        self._test_equals_args(mock_cliargs, args)
