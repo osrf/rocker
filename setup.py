7da55bf452222d909c73efd8a1a6b7e93f8657e3
Tully Foote <tullyfoote@intrinsic.ai>
2023-05-04 22:46:21 +0000

    iterating to try to get docker working
    Signed-off-by: Tully Foote <tullyfoote@intrinsic.ai>

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ install_requires = [
     'empy',
     'pexpect',
     'packaging',
+    'urllib3<2', # Workaround for https://github.com/docker/docker-py/issues/3113
 ]
 
 # docker API used to be in a package called `docker-py` before the 2.0 release
