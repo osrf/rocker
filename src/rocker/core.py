db03937830a1d77f5fda0e44acc6570659662dd4
jQuery Dark theme project <cr07fk14@gmail.com>
2023-05-05 03:22:46 +0530

    issue #35 resolved - review required

--- src/rocker/core.py
+++ src/rocker/core.py
@@ -135,9 +135,11 @@ def get_docker_client():
 
 def docker_build(docker_client = None, output_callback = None, **kwargs):
     image_id = None
+    build_success = False
 
     if not docker_client:
         docker_client = get_docker_client()
+
     kwargs['decode'] = True
     for line in docker_client.build(**kwargs):
         output = line.get('stream', '').rstrip()
@@ -150,12 +152,12 @@ def docker_build(docker_client = None, output_callback = None, **kwargs):
         match = re.match(r'Successfully built ([a-z0-9]{12})', output)
         if match:
             image_id = match.group(1)
-
-    if image_id:
+            build_success = True
+            
+    if build_success:
         return image_id
     else:
-        print("no more output and success not detected")
-        return None
+        raise Exception("Build failed: no more output and success not detected")
 
 
 class SIGWINCHPassthrough(object):
