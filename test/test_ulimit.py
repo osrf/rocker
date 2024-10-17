4be243630c5c15d1e3ca5148e2ab2396796e8540
Felipe Padula Sanches <fpadula92@gmail.com>
2024-10-18 12:23:13 +1300

    Return tuple instead of bool for better unit test report feedback

--- test/test_ulimit.py
+++ test/test_ulimit.py
@@ -29,72 +29,72 @@ class UlimitTest(unittest.TestCase):
 
     def _is_arg_translation_ok(self, mock_cliargs, expected):
         is_ok = False
+        message_string = ""
         try:
             docker_args = self._instance.get_docker_args(
                 {self._instance.get_name(): [mock_cliargs]})
             is_ok = docker_args == expected
-            print(f"DEBUG: Expected docker_args: {expected}")
-            print(f"DEBUG: Resulted docker_args: {docker_args}")
+            message_string = f"Expected: '{expected}', got: '{docker_args}'"
         except ArgumentTypeError:
-            print("DEBUG: Incorrect argument format")
-        return is_ok
+            message_string = "Incorrect argument format"
+        return (is_ok, message_string)
 
     def test_args_single_soft(self):
         """Test single soft limit argument."""
         mock_cliargs = ["rtprio=99"]
         expected = " --ulimit rtprio=99"
-        self.assertTrue(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertTrue(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_multiple_soft(self):
         """Test multiple soft limit arguments."""
         mock_cliargs = ["rtprio=99", "memlock=102400"]
         expected = " --ulimit rtprio=99 --ulimit memlock=102400"
-        self.assertTrue(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertTrue(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_single_hard(self):
         """Test single hard limit argument."""
         mock_cliargs = ["nofile=1024:524288"]
         expected = " --ulimit nofile=1024:524288"
-        self.assertTrue(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertTrue(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_multiple_hard(self):
         """Test multiple hard limit arguments."""
         mock_cliargs = ["nofile=1024:524288", "rtprio=90:99"]
         expected = " --ulimit nofile=1024:524288 --ulimit rtprio=90:99"
-        self.assertTrue(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertTrue(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_multiple_mix(self):
         """Test multiple mixed limit arguments."""
         mock_cliargs = ["rtprio=99", "memlock=102400", "nofile=1024:524288"]
         expected = " --ulimit rtprio=99 --ulimit memlock=102400 --ulimit nofile=1024:524288"
-        self.assertTrue(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertTrue(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_wrong_single_soft(self):
         """Test if single soft limit argument is wrong."""
         mock_cliargs = ["rtprio99"]
         expected = " --ulimit rtprio99"
-        self.assertFalse(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertFalse(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_wrong_multiple_soft(self):
         """Test if multiple soft limit arguments are wrong."""
         mock_cliargs = ["rtprio=99", "memlock102400"]
         expected = " --ulimit rtprio=99 --ulimit memlock=102400"
-        self.assertFalse(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertFalse(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_wrong_single_hard(self):
         """Test if single hard limit arguments are wrong."""
         mock_cliargs = ["nofile=1024:524288:"]
         expected = " --ulimit nofile=1024:524288"
-        self.assertFalse(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertFalse(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_wrong_multiple_hard(self):
         """Test if multiple hard limit arguments are wrong."""
         mock_cliargs = ["nofile1024524288", "rtprio=90:99"]
         expected = " --ulimit nofile=1024:524288 --ulimit rtprio=90:99"
-        self.assertFalse(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertFalse(*self._is_arg_translation_ok(mock_cliargs, expected))
 
     def test_args_wrong_multiple_mix(self):
         """Test if multiple mixed limit arguments are wrong."""
         mock_cliargs = ["rtprio=:", "memlock102400", "nofile1024:524288:"]
         expected = " --ulimit rtprio=99 --ulimit memlock=102400 --ulimit nofile=1024:524288"
-        self.assertFalse(self._is_arg_translation_ok(mock_cliargs, expected))
+        self.assertFalse(*self._is_arg_translation_ok(mock_cliargs, expected))
