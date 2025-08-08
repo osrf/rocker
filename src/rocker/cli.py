46f0a6686da76de13d95efb2f2dd8dad9d10d536
Tully Foote <tullyfoote@intrinsic.ai>
2025-08-08 11:48:27 -0700

    Update src/rocker/cli.py
    Co-authored-by: Scott K Logan <logans@cottsay.net>

--- src/rocker/cli.py
+++ src/rocker/cli.py
@@ -83,7 +83,7 @@ def main():
     # Right now the printed results will include '-it'
     # But based on testing the --detach overrides -it in docker so it's ok.
 
-    # Default to non-interactive if unset
+    # Default to interactive if unset
     if args_dict.get('mode') not in OPERATION_MODES:
         print("Mode unset, defaulting to interactive")
         args_dict['mode'] = OPERATIONS_INTERACTIVE
